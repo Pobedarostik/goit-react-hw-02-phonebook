5e41fa2b8feab9ebbd0462718981d02a24c7f962
Rostyslav Pobidynskyi <pobedarostik@gmail.com>
2021-10-13 22:05:45 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 
 import React, { Component } from "react";
-import ContactForm  from './ContactForm/ContactForm '
+import ContactForm from './ContactForm/ContactForm '
 import { v4 as uuidv4 } from "uuid";
 import ContactList from './ContactList/ContactList'
 import Filter from './Filter/Filter'
 
 
 export default class App extends Component {
-   state = {
-     contacts: [
+  state = {
+    contacts: [
       { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
       { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
       { id: "id-3", name: "Eden Clements", number: "645-17-79" },
       { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-     ],
-     filter: '',
+    ],
+    filter: '',
   };
-  
+
 
   forForm = data => {
     const searchSameName = this.state.contacts
@@ -31,21 +31,21 @@ export default class App extends Component {
       const contact = {
         ...data,
         id: uuidv4(),
-   };
-     
+      };
+
       this.setState(({ contacts }) => ({
         contacts: [contact, ...contacts],
       }));
     }
 
-   
+
   }
-  
+
   changeFilter = (filter) => {
     this.setState({ filter });
   };
 
-  
+
   getContactName = () => {
     const { contacts, filter } = this.state;
 
@@ -54,28 +54,26 @@ export default class App extends Component {
     );
   };
 
-    removeContact = (contactId) => {
+  removeContact = (contactId) => {
     this.setState((prevState) => {
       return {
         contacts: prevState.contacts.filter(({ id }) => id !== contactId),
       };
     });
   };
- 
 
-render() {
-  const contactName = this.getContactName();
-  const { filter } = this.state;
+
+  render() {
+    const contactName = this.getContactName();
+    const { filter } = this.state;
 
     return (
       <div>
         <h1>Phonebook</h1>
-        <ContactForm  onContact={this.forForm} />
+        <ContactForm onContact={this.forForm} />
         <h2>Contacts</h2>
-        {contactName.length > 1 && (
-          <Filter value={filter} onChangeFilter={this.changeFilter} />
-        )}
-         {contactName.length > 0 ? (
+        <Filter value={filter} onChangeFilter={this.changeFilter} />
+        {contactName.length > 0 ? (
           <ContactList
             contacts={contactName}
             onRemoveContact={this.removeContact}
@@ -83,9 +81,9 @@ render() {
         ) : (
           <p>Your phonebook is empty. Please add contact.</p>
         )}
-        
-        </div>
-      
+
+      </div>
+
     );
   }
 }
